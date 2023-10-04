d1e2c8e6e05ec30f7ffc2e73cd0652370414b1af
yarrobong <oneboardsholi@gmail.com>
2023-10-05 00:25:40 +0500

    Добавьте файлы проекта.

---
+++ 04.10TaskHOme/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static bool IsSmiley(string input)
+    {
+        // Паттерн смайлика
+        string pattern = @"^[;:]+-*[()\[\]]+$";
+
+        // Создаем регулярное выражение и проверяем строку
+        return Regex.IsMatch(input, pattern);
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите смайлик: ");
+        string input = Console.ReadLine();
+        if (IsSmiley(input))
+        {
+            Console.WriteLine("Это смайлик!");
+        }
+        else
+        {
+            Console.WriteLine("Это не смайлик.");
+        }
+    }
+}
